fafdd335dc269ed37bb8518e1131134d36f790d6
Frank Shan <frankqs0304@hotmail.com>
2021-11-27 23:32:49 +0800

    update format

--- search_file_in_path/search_file_in_path.py
+++ search_file_in_path/search_file_in_path.py
@@ -3,55 +3,81 @@
 
 import os
 
-def find_files_01(filename, search_path):
+def search_files(filename, search_path):
     result = []
     # Wlaking top-down from the root
     #for root, dir, files in os.walk(search_path):
     for path, subdir, files in os.walk(search_path):
         if filename in files:
             result.append(os.path.join(path, filename))
-            print ("get file info " + path)
+            #debug#print ("Get file info " + path)
     return result
 
-def find_path_01(pathname, search_path):
+def search_path(pathname, search_path):
     result = []
     # Wlaking top-down from the root
     #for root, dir, files in os.walk(search_path):
     for path, subdir, files in os.walk(search_path):
         #print (".")
         if pathname in subdir:
+            #debug#
+            #debug#print ("------------------------------")
+            #debug#print ("Get path name of file   : " + pathname)
+            #debug##print ("Get subdir info        : ")
+            #debug##print (subdir)
+            #debug#print ("------------------------------")
             #result.append(os.path.join(path, filename))
             result.append(path)
-            print ("get path info one time" + path)
+            #debug#print ("Get path info one time" + path)
     return result
 
 
-def find_file(file_name, directory_name):
+def search_file_01(file_name, directory_name):
     files_found = []
     for path, subdirs, files in os.walk(directory_name):
         for name in files:
             if(file_name == name):
-                file_path = os.path.join(path,name)
-                print ("get file info " + file_path)
-                files_found.append(file_path)
+                file_name_with_path = os.path.join(path,name)
+                #debug#
+                #debug#print ("------------------------------")
+                #debug#print ("Get file name with path : " + file_name_with_path)
+                #debug#print ("Get path name of file   : " + path)
+                #debug##print ("Get subdirs info        : ")
+                #debug##print (subdirs)
+                #debug#print ("Get file name info      : " + name)
+                #debug#print ("------------------------------")
+                files_found.append(file_name_with_path)
     return files_found
 
 def main():
-    file_got_after_search       =[]
+    file_got_after_search = []
     #['array1' ,'array2']
-    #print (find_file('study.txt', '~/data/myenv'))
-    #print (find_file('study.txt', '/home/fshan/data/myenv'))
-    file_got_after_search+=(find_file('study.txt', '/home/fshan/data/myenv'))
+    #print (search_file('study.txt', '~/data/myenv'))
+    #print (search_file('study.txt', '/home/fshan/data/myenv'))
+    file_got_after_search+=(search_file_01('_emacs', '/home/fshan/data/myenv'))
+    print ("Get the [files] :")
     print (file_got_after_search)
 
     #file01_got_after_search = []
-    #file01_got_after_search.append(find_files_01('study.txt', '/home/fshan/data/myenv'))
-    file_got_after_search+=(find_files_01('study.txt', '/home/fshan/data/myenv'))
+    #file01_got_after_search.append(search_files_01('study.txt', '/home/fshan/data/myenv'))
+    file_got_after_search+=(search_files('study.txt', '/home/fshan/data/myenv'))
+    print ("Get the [files] :")
     print (file_got_after_search)
 
+    file_got_after_search+=(search_files('matchit.vim', '/home/fshan/data/myenv'))
+    print ("Get the [files] :")
+    print (file_got_after_search)
+
+    #
+
     path01_got_after_search = []
-    #path01_got_after_search.append(find_path_01('_vim', '/home/fshan/data/myenv'))
-    path01_got_after_search+=(find_path_01('_vim', '/home/fshan/data/myenv'))
+    #path01_got_after_search.append(search_path_01('_vim', '/home/fshan/data/myenv'))
+    path01_got_after_search+=(search_path('_vim', '/home/fshan/data/myenv'))
+    print ("Get the [paths] :")
+    print (path01_got_after_search)
+
+    path01_got_after_search+=(search_path('pyim', '/home/fshan/data/myenv'))
+    print ("Get the [paths] :")
     print (path01_got_after_search)
 
 
